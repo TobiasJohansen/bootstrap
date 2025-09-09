81885d045af46e5e474cfd5a389e30a62ae8af53
Tobias Johansen <tobjoh1994@gmail.com>
2025-09-09 23:48:38 +0200

    revert packages

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ locale-gen >/dev/null
 echo "==> Installing packages..."
 
 pacman -Syu --noconfirm >/dev/null
-pacman -S --noconfirm fd gcc git make man-db neovim openssh ripgrep starship stow unzip xclip >/dev/null
+pacman -S --noconfirm base-devel fd git man-db neovim openssh ripgrep starship stow unzip xclip >/dev/null
 
 if ! command -v pnpm &>/dev/null; then
   curl -fsSL https://get.pnpm.io/install.sh | sh - >/dev/null
