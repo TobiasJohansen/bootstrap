244c4d4db590d1d8f9a38eb19556810bb9a37de2
Tobias Johansen <tobjoh1994@gmail.com>
2025-09-09 10:43:08 +0200

    replace the override flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,11 +11,12 @@ pacman -Syu --noconfirm >/dev/null
 pacman -S --noconfirm base-devel git man-db neovim openssh starship stow unzip xclip >/dev/null
 
 curl -fsSL https://get.pnpm.io/install.sh | sh - >/dev/null
+rm ~/.bashrc
 
 echo "==> Setting up dotfiles..."
 
 git clone https://github.com/TobiasJohansen/dotfiles ~/dotfiles >/dev/null
-stow -d ~/dotfiles -t ~ . --override='.*' >/dev/null
+stow -d ~/dotfiles -t ~ . >/dev/null
 source ~/.bashrc
 
 # pnpm env use --lts >/dev/null
