6ed0738319004d4d50af3c2031f7fbeb26c5d9be
Tobias Johansen <tobjoh1994@gmail.com>
2025-09-09 09:57:31 +0200

    add pnpm install

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,9 @@ echo "==> Installing packages..."
 pacman -Syu --noconfirm >/dev/null
 pacman -S --noconfirm base-devel git man-db neovim openssh starship stow unzip xclip >/dev/null
 
+curl -fsSL https://get.pnpm.io/install.sh | sh - >/dev/null
+pnpm env use --lts
+
 echo "==> Setting up dotfiles..."
 
 git clone https://github.com/TobiasJohansen/dotfiles ~/dotfiles >/dev/null
