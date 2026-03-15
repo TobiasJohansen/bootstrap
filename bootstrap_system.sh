ef92e1e6f5ced05e289bb2002a45058d12369623
Tobias Johansen <tobjoh1994@gmail.com>
2026-03-15 19:27:32 +0100

    add comment about how to add user as default in wsl

--- bootstrap_system.sh
+++ bootstrap_system.sh
@@ -10,12 +10,15 @@ echo "==> Installing packages..."
 pacman -Syu --noconfirm >/dev/null
 pacman -S --noconfirm base-devel fd git keychain lazygit man-db neovim openssh pnpm ripgrep starship stow tree-sitter-cli unzip xclip >/dev/null
 
-echo "==> Setting up user..."
-
 USERNAME="tobias"
 if ! id "$USERNAME" &>/dev/null; then
+  echo "==> Adding user..."
   useradd -m -G wheel -k /var/empty -s /bin/bash "$USERNAME"
   sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+  if [ -v WSL_DISTRO_NAME ]; then
+    echo "Exit WSL and run the following command to set '$USERNAME' as the default user"
+    echo "wsl --manage archlinux --set-default-user $USERNAME"
+  fi
 fi
 
 echo "==> Bootstrap complete"
